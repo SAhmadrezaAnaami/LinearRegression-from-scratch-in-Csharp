03b5b965aa0674a27950bea82a9fdd073b26ba40
AhmadReza_Anaami <47621957+SAhmadrezaAnaami@users.noreply.github.com>
2023-12-02 21:49:22 +0330

    Add files via upload

---
+++ LinearRegression/LinearRegression.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearRegression
+{
+    internal class LinearRegression
+    {
+        double iteration;
+        double learningRate;
+        double weight;
+        double bias;
+        bool verbose;
+        public LinearRegression(double iteration, double learningRate)
+        {
+            this.iteration = iteration;
+            this.learningRate = learningRate;
+            this.verbose = true;
+        }
+
+        public LinearRegression(double iteration, double learningRate, bool verbose) : this(iteration, learningRate)
+        {
+            this.verbose = verbose;
+        }
+
+        public void fit(double[] X , double[] Y)
+        {
+            this.weight = 0;
+            this.bias = 0;
+
+            for (int i = 0; i < this.iteration; i++)
+            {
+                double dw = 0;
+                double db = 0;
+                double loss = 0;
+                for (int j = 0; j < X.Length; j++)
+                {
+                    double sum = 1d / X.Length;
+                    double y_pred = this.predictForFit(X[j]);
+                    loss = y_pred - Y[j];
+                    dw += sum * loss * X[j];
+                    db += sum * loss;
+
+                }
+                if (this.verbose)
+                {
+                    Console.WriteLine("epoch : " + i + " -- loss : " + Math.Abs(loss));
+                }
+                this.weight -= this.learningRate * dw;
+                this.bias -= this.learningRate * db;
+                
+            }
+
+
+        }
+        private double predictForFit(double x)
+        {
+            return this.weight * x + this.bias;
+        }
+        public double predict(double x , Boolean Round)
+        {
+            if (Round)
+                return Math.Round(this.weight * x + this.bias);
+            else
+                return this.weight * x + this.bias;
+        }
+        public void printWeights()
+        {
+            Console.WriteLine("weight : " + this.weight + " -- bias : " + this.bias);
+        }
+    }
+}
---
+++ LinearRegression/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearRegression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] x = { 1, 2, 3, 4 ,5,6,7,8,9 };
+            double[] y = { 3,5,7,9,11,13,15,17,19 };
+            LinearRegression lm = new LinearRegression(5000, 0.01);
+            lm.fit(x, y);
+            Console.WriteLine(lm.predict(10 , false));
+            lm.printWeights();
+            Console.ReadLine();
+        }
+    }
+}
